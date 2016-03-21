ce2270e2c3d5131730b257029bf2f3a2d02e5e8e
David Woldenberg <dwoldenberg1@gmail.com>
2016-03-21 18:13:49 -0500

    second upload

---
+++ datatype_sizes.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+	printf("The 'int' data type is\t\t %d bytes\n", sizeof(int));
+	printf("same for unsigned int, short int, long int, long long int, float, and char pg 43\t");
+}
