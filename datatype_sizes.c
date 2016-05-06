678bb1ef902675c5eaf2aab4633fb201a3803aeb
David Woldenberg <dwoldenberg1@gmail.com>
2016-05-06 12:17:56 -0500

    some stale changes

--- datatype_sizes.c
+++ datatype_sizes.c
@@ -2,5 +2,10 @@
 
 int main(){
 	printf("The 'int' data type is\t\t %d bytes\n", sizeof(int));
-	printf("same for unsigned int, short int, long int, long long int, float, and char pg 43\t");
+	printf("The 'unsigned int' data type is\t %d bytes\n", sizeof(unsigned int));
+	printf("The 'short int' data type is\t %d bytes\n", sizeof(short int));
+	printf("The 'long int' data type is\t %d bytes\n", sizeof(long int));
+	printf("The 'long long int' data type is %d bytes\n", sizeof(long long int));
+	printf("The 'float' data type is\t %d bytes\n", sizeof(float));
+	printf("The 'char' data type is\t\t %d bytes\n", sizeof(char));
 }
